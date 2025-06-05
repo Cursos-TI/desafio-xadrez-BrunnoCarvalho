3b41649618311b3299e719d514bb2f9c2fcb8668
Bruno Carvalho <brunno_carvalho@outlook.com>
2025-06-05 23:04:15 +0000

    Conclusão nível mestre.

--- xadrez.c
+++ xadrez.c
@@ -3,6 +3,35 @@
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+void movimentoTorre(int casas){
+    if(casas > 0){
+        printf("Direita\n");
+        movimentoTorre(casas - 1);
+    }
+}
+
+void movimentoRainha(int casas){
+    if(casas > 0){
+        printf("Esquerda\n");
+        movimentoTorre(casas - 1);
+    }
+ }
+
+ void movimentoBispo(int casasBispo){
+
+    if(casasBispo <= 0)
+        return;
+
+    for(int i = 0; i < 1; i++){
+        printf("Cima, ");
+        for(int j = 0; j < 1; j++){
+            printf("Direita \n");
+        }
+    }
+
+    movimentoBispo(casasBispo - 1);
+
+ }
 
 int main() {
     // Nível Novato - Movimentação das Peças
@@ -58,12 +87,45 @@ int main() {
         printf("Esquerda \n");
     }
 
+    printf("\n");
+
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
 
+    printf("Movimento da torre recursivo: \n");
+    movimentoTorre(casasTorre);
+    printf("\n");
+
+    printf("Movimento da rainha recursivo: \n");
+    movimentoRainha(casasRainha);
+    printf("\n");
+
+    printf("Movimento do bispo recursivo: \n");
+    movimentoBispo(casasBispo);
+    printf("\n");
+
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
+    printf("Movimento do cavalo: \n");
+    for(int vertical = 0, horizontal = 0; horizontal < cavaloHorizontal + 1; horizontal++){
+        
+        //horizontal está sendo percorrida 3x, mas imprimimos apenas quando for horizontal = 1
+        if(horizontal == 0){  //Criado apenas para fazer o pedido do exercício, continuando o for quando horizontal = 0
+            continue;
+        }
+
+        if(horizontal == 2){  //Neste caso é um a mais do que gostariamos de andar na horizontal, então fazemos break para encerrar
+            break;
+        }
+                
+        while(vertical < cavaloVertical){
+            printf("Cima, ");
+            vertical++;
+        }
+
+        printf("Direita \n");
+    }
 
     return 0;
 }
